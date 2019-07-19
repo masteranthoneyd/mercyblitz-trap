3f6f79364c8495cf5a3ec6cf4db03c7c6afd1f5e
yangbingdong <yangbingdong1994@gmail.com>
2019-07-19 16:41:01 +0800

    Add Trap009

--- src/main/java/Trap005_Reflector_Morph01.java
+++ src/main/java/Trap005_Reflector_Morph01.java
@@ -1,5 +1,4 @@
 import java.lang.reflect.Field;
-import java.lang.reflect.Modifier;
 
 /**
  * @author ybd
@@ -11,26 +10,16 @@ import java.lang.reflect.Modifier;
  * (b) GuangZhou
  * (c) Beijing
  * (d) 以上都不是
- * <p>
- *
- * Inline 原理
  */
-public class Trap005_Reflector_Ext02 {
+public class Trap005_Reflector_Morph01 {
     public static void main(String[] args) throws Exception {
         Field nameField = OneCity.class.getDeclaredField("name");
-
-        Field modifiersField = Field.class.getDeclaredField("modifiers");
-        modifiersField.setAccessible(true);
-        modifiersField.setInt(nameField, nameField.getModifiers() & ~Modifier.FINAL);
-
         nameField.setAccessible(true);
         nameField.set(null, "GuangZhou");
         System.out.println(OneCity.getName());
     }
-
     static class OneCity {
         private static final String name = "Beijing";
-
         public static String getName() {
             return name;
         }
--- src/main/java/Trap005_Reflector_Morph02.java
+++ src/main/java/Trap005_Reflector_Morph02.java
@@ -1,4 +1,5 @@
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 /**
  * @author ybd
@@ -10,16 +11,26 @@ import java.lang.reflect.Field;
  * (b) GuangZhou
  * (c) Beijing
  * (d) 以上都不是
+ * <p>
+ *
+ * Inline 原理
  */
-public class Trap005_Reflector_Ext01 {
+public class Trap005_Reflector_Morph02 {
     public static void main(String[] args) throws Exception {
         Field nameField = OneCity.class.getDeclaredField("name");
+
+        Field modifiersField = Field.class.getDeclaredField("modifiers");
+        modifiersField.setAccessible(true);
+        modifiersField.setInt(nameField, nameField.getModifiers() & ~Modifier.FINAL);
+
         nameField.setAccessible(true);
         nameField.set(null, "GuangZhou");
         System.out.println(OneCity.getName());
     }
+
     static class OneCity {
         private static final String name = "Beijing";
+
         public static String getName() {
             return name;
         }
